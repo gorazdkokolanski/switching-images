9288ff74b946fd8742de625d35ba8aa3664a77fb
gorazdkokolanski <130471310+gorazdkokolanski@users.noreply.github.com>
2024-12-07 20:20:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,154 +1,68 @@
-const photoDivs = [
-    `
-    <div>
-        <img src="img/1.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/2.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/3.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/4.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/5.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/6.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/7.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/8.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/9.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/10.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/11.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/12.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/13.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/14.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/15.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/16.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/17.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/18.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/19.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/20.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/21.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/22.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/23.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/24.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/25.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/26.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/27.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/28.jpg" alt="photo">
-    </div>
-    `,
-    `
-    <div>
-        <img src="img/29.jpg" alt="photo">
-    </div>
-    `
+const imagePaths = [
+    "img/1.jpg",
+    "img/2.jpg",
+    "img/3.jpg",
+    "img/4.jpg",
+    "img/5.jpg",
+    "img/6.jpg",
+    "img/7.jpg",
+    "img/8.jpg",
+    "img/9.jpg",
+    "img/10.jpg",
+    "img/11.jpg",
+    "img/12.jpg",
+    "img/13.jpg",
+    "img/14.jpg",
+    "img/15.jpg",
+    "img/16.jpg",
+    "img/17.jpg",
+    "img/18.jpg",
+    "img/19.jpg",
+    "img/20.jpg",
+    "img/21.jpg",
+    "img/22.jpg",
+    "img/23.jpg",
+    "img/24.jpg",
+    "img/25.jpg",
+    "img/26.jpg",
+    "img/27.jpg",
+    "img/28.jpg",
+    "img/29.jpg"
 ];
+
+const photoDivs = imagePaths.map(path => {
+    return `
+        <div>
+            <img src="${path}" alt="photo">
+        </div>
+    `;
+});
+
+function preloadImages(paths, callback) {
+    let loadedCount = 0;
+    const totalCount = paths.length;
+    paths.forEach((src) => {
+        const img = new Image();
+        img.onload = () => {
+            loadedCount++;
+            if (loadedCount === totalCount) {
+                callback();
+            }
+        };
+        img.onerror = () => {
+            console.warn(`Failed to load image: ${src}`);
+            loadedCount++;
+            if (loadedCount === totalCount) {
+                callback();
+            }
+        };
+        img.src = src;
+    });
+}
 
 const widths = [
     20, 45, 45, 20, 20, 20, 25, 20, 20, 25, 20, 25, 20, 20, 20,
-    45, 20, 25, 20, 25, 20, 25, 22, 22, 22, 22, 22, 20, 20
+    45, 20, 25, 20, 25, 40, 25, 22, 22, 22, 22, 22, 20, 20
 ];
 
 let lastX = null;
@@ -209,3 +123,5 @@ function addNewDiv(x, y) {
     newDiv.innerHTML = photoDivs[iterator];
     document.querySelector(".main").appendChild(newDiv);
 }
+
+preloadImages(imagePaths, startInteraction);
